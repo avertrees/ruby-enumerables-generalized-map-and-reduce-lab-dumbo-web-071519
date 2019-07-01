dc7b9eef0da9da955f5a56ff97ed88e2068debc3
Alessandra Vertrees <avertrees447@gmail.com>
2019-07-01 14:25:38 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -41,3 +41,50 @@ make_sandwich("Creamy peanut butter", "glittering sense of accomplishment") { |b
 # Lewis Carroll's Kitchen
 make_sandwich("Creamy peanut butter", "glittering sense of accomplishment") { |b| "#{b.reverse} on #{"bread".reverse}" }
 "hciwdnas tnemhsilpmocca fo esnes gnirettilg dna rettub tunaep ymaerC A on daerb"
+
+
+require 'spec_helper'
+
+describe 'my own map' do
+  it "returns an array with all values made negative" do
+    expect(map([1, 2, 3, -9]){|n| n * -1}).to eq([-1, -2, -3, 9])
+  end
+
+  it "returns an array with the original values" do
+    dune = ["paul", "gurney", "vladimir", "jessica", "chani"]
+    expect(map(dune){|n| n}).to eq(dune)
+  end
+
+  it "returns an array with the original values multiplied by 2" do
+    expect(map([1, 2, 3, -9]){|n| n * 2}).to eq([2, 4, 6, -18])
+  end
+
+  it "returns an array with the original values squared" do
+    expect(map([1, 2, 3, -9]){|n| n * n}).to eq([1, 4, 9, 81])
+  end
+end
+
+describe 'my own reduce' do
+  it "returns a running total when not given a starting point" do
+    source_array = [1,2,3]
+    expect(reduce(source_array){|memo, n| memo + n}).to eq(6)
+  end
+
+  it "returns a running total when given a starting point" do
+    source_array = [1,2,3]
+    starting_point = 100
+    expect(reduce(source_array, starting_point){|memo, n| memo + n}).to eq(106)
+  end
+
+  it "returns true when all values are truthy" do
+    source_array = [1, 2, true, "razmatazz"]
+    expect(reduce(source_array){|memo, n| memo && n}).to be_truthy
+  end
+
+it "returns false when any value is false" do
+    source_array = [1, 2, true, "razmatazz", false]
+    expect(reduce(source_array){|memo, n| memo && n}).to be_falsy
+  end
+
+reduce(source_array){|memo, n| memo || n}
+reduce(source_array){|memo, n| memo && n}
